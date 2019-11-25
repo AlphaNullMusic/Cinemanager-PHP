d8b05e42ea81d413e068e9a53c5478be9971314d
AlphaNullMusic <alphanullmusic@gmail.com>
2019-11-25 17:30:07 +1300

    Update about-us.php

--- web/about-us.php
+++ web/about-us.php
@@ -12,9 +12,8 @@ if(!$smarty->isCached($tpl,$cache_id)) {
 	$smarty->assign('domain',$cinema_domain);
 	$smarty->assign('name',$cinema_data['cinema_name']);
 	$smarty->assign('city',$cinema_data['city']);
-	$smarty->assign('movie_image_url',$global['movie_image_url']);
-	$smarty->assign('movie_image_url_secure',$global['movie_image_url_secure']);
 	$smarty->assign('tpl_name',$tpl_name);
+	$smarty->assign('gacode',$config['ga_code']);
 	
 	if (has_permission('edit_pages')) {
 		$smarty->assign('page',get_page_content('about-us'));
@@ -22,11 +21,6 @@ if(!$smarty->isCached($tpl,$cache_id)) {
 
 	// Common functions
 	include('inc/local.inc.php');
-	
-	// Register functions / filters
-	//$smarty->registerPlugin("function", "summary", "smarty_summary");
-	//$smarty->registerFilter("pre", "edit_image_path");
-
 }
 
 // Include template
