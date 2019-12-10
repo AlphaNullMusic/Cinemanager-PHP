e8b5753abc492c851b0769f5f24307cd834a3c49
Roman Avery <roman@shoreline.nz>
2019-12-10 17:35:02 +1300

    Update

---
+++ web/tpl/cache/404^29a84f3d9bcb4364d9f6873a93abd77d9d14e9f5.404.tpl.php
@@ -0,0 +1,101 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-12-10 16:58:02
+  from '/var/www/Cinemanager/web/tpl/404.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5def17ca4d97a5_07879858',
+  'has_nocache_code' => true,
+  'file_dependency' => 
+  array (
+    'cc9a9c9f7a0917a1a53ea197fc99e7d8293de378' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/404.tpl',
+      1 => 1575885459,
+      2 => 'file',
+    ),
+    '12f3ac630680bc6778753de8bb98e13015ec9d16' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/inc/tpl/footer.tpl',
+      1 => 1575886306,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 600,
+),true)) {
+function content_5def17ca4d97a5_07879858 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en-nz">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>404 - Page Not Found</title>
+<meta name="description" content="Shoreline Cinema is Waikanae's premiere cinema complex. Find out who we are, what's on, what's coming up, how to contact us and much more.">
+<link href="/tpl/inc/css/styles.css" rel="stylesheet" type="text/css" />
+<link href="/tpl/inc/css/print.css" rel="stylesheet" type="text/css" media="print" />
+</head>
+<body>
+    <header>  
+      <div class="box-auto hide show-med"><img src="/tpl/inc/img/sl_logo.png" class="img-responsive mc-auto show"></div>
+      
+      <nav>
+        <a class="logo hide-med" href="#"><img src="/tpl/inc/img/sl_logo.png" height="25"></a>
+        <button class="hide-med" type="button">
+            <span class="icon-text">Menu&nbsp;</span>
+            <span class="icon"></span>
+        </button>
+        
+        
+        <div class="collapse jc-center" id="navbar">
+            <ul class="nav-links">
+                <li class="nav-item">
+                    <a class="nav-link " href="/home/">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/whats-on-today/">WHAT&apos;S ON TODAY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/whats-on/">WHAT&apos;S ON</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/coming-soon/">COMING SOON</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/venue-hire/">VENUE HIRE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/about-us/">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/contact-us/">CONTACT US</a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </header>
+    
+    <div class="wrapper">
+			
+
+    
+    <div class="information">
+        <div class="content">
+            <div class="content-wrapper">
+                <div class="box">
+                    <h2>Sorry... we couldn't find the page you were looking for.</h2>
+	                <p><?php echo '<?=';?>($_REQUEST['er'])?'Error: '.$_REQUEST['er']:'';<?php echo '?>';?></p>
+	                <p></p>
+	                <p>Click the button below to go back home.</p>
+	                <a class="btn dark" href="/home/">Home</a>
+                </div>
+            </div>    
+        </div>
+    </div>
+
+    <footer><div id="text"><p>Web design and content &copy; 2019, Shoreline Cinema Waikanae, New Zealand. <a id="improve-visibility">Improve Visibility.</a></p></div></footer></div><script src="/tpl/inc/js/jquery-3.4.1.min.js"></script><script src="/tpl/inc/js/js.cookie-2.2.1.min.js"></script><script src="/tpl/inc/js/scripts.js"></script><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-137475424-2"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-137475424-2');</script></body></html>
+
+
+<?php }
+}
---
+++ web/tpl/cache/about_us^202f4678c1f61a7d77d82c1e63ddbf1d808a7083.generic.tpl.php
@@ -0,0 +1,60 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-12-10 11:53:33
+  from '/var/www/Cinemanager/web/tpl/generic.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5deed06dc4fe70_31552589',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'eaf2bc4d279f500c00286cdb0705b00eb361c29e' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/generic.tpl',
+      1 => 1574633434,
+      2 => 'file',
+    ),
+    '2ddc8f9c3e6633867c3e519bcee932881794a66f' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/inc/tpl/header.tpl',
+      1 => 1575885725,
+      2 => 'file',
+    ),
+    '12f3ac630680bc6778753de8bb98e13015ec9d16' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/inc/tpl/footer.tpl',
+      1 => 1575886306,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 600,
+),true)) {
+function content_5deed06dc4fe70_31552589 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html><html lang="en-nz"><head><meta http-equiv="Content-Type" content="text/html;charset=UTF-8" /><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1"><title>About Us</title><meta name="description" content="Shoreline Cinema is Waikanae's premiere cinema complex. Find out who we are, what's on, what's coming up, how to contact us and much more."><link href="/tpl/inc/css/styles.css" rel="stylesheet" type="text/css" /><link href="/tpl/inc/css/print.css" rel="stylesheet" type="text/css" media="print" /></head><body><header><div class="box-auto hide show-med"><img src="/tpl/inc/img/sl_logo.png" class="img-responsive mc-auto show"></div><nav><a class="logo hide-med" href="#"><img src="/tpl/inc/img/sl_logo.png" height="25"></a><button class="hide-med" type="button"><span class="icon-text">Menu&nbsp;</span><span class="icon"></span></button><div class="collapse jc-center" id="navbar"><ul class="nav-links"><li class="nav-item"><a class="nav-link " href="/home/">HOME</a></li><li class="nav-item"><a class="nav-link " href="/whats-on-today/">WHAT&apos;S ON TODAY</a></li><li class="nav-item"><a class="nav-link " href="/whats-on/">WHAT&apos;S ON</a></li><li class="nav-item"><a class="nav-link " href="/coming-soon/">COMING SOON</a></li><li class="nav-item"><a class="nav-link " href="/venue-hire/">VENUE HIRE</a></li><li class="nav-item"><a class="nav-link active" href="/about-us/">ABOUT US</a></li><li class="nav-item"><a class="nav-link " href="/contact-us/">CONTACT US</a></li></ul></div></nav></header><div class="wrapper">
+<div class="information"><h1>About Us</h1><p>The Shoreline Cinema is an independent cinema established in the seaside town of Waikanae in 2008. The owner Peter Avery is a Kapiti local with a passion for film and hospitality.&nbsp;</p>
+<p>Featuring an eclectic mix of films around the world that both inspire and entertain, Peter has not only brought his passion for good film to the Kapiti coast, but also the experience that encapsulates boutique cinema.</p>
+<p>Featuring a contemporary selection of NZ wines, the sumptuous Rangitoto espresso blend from Allpress Coffee, and the award winning range of Tuatara beers made by the blokes just up the road. Throw into the mix iconic delectables such as Oob Organic Ice Cream, and a range of fresh organic teas and beverages. Making the experience more enjoyable we have an assortment of bakery delights from the clever ladies at Tom Thumb.</p>
+<p>For those of you who enjoy a more technical spin, our cinema features two state of the art auditoriums, equipped with NEC electronic digital projection. Both theatres are fully air-conditioned and feature 7.1 Dolby surround sound. They are also equipped with the latest hearing-impaired technologies.</p>
+<p>You can enjoy all this in our supremely comfortable couches, especially designed for our theatre.</p>
+<p>The Shoreline offers a constantly changing selection of the best films we can find, so please keep checking into this site for updates of screenings coming movies.</p>
+<p>The Shoreline crew are proud of our cinema and look forward to entertaining you soon.</p>
+<p>&nbsp;</p>
+<hr />
+<p>&nbsp;</p>
+<p><strong>Ticket Prices</strong></p>
+<p>&nbsp;</p>
+<p><strong>Adults</strong> <br />All Seats: $17.00</p>
+<p>&nbsp;</p>
+<p><strong>Students and Senior Citizen</strong><br />All Seats: $13.50</p>
+<p>&nbsp;</p>
+<p><strong>Children</strong>&nbsp;(ten years and under)<br />All Seats: $10.00</p>
+<p>&nbsp;</p>
+<p><strong>Met Opera</strong><br />Adult: $33<br />Concession: $28</p>
+<p>&nbsp;</p>
+<p><strong>National Theatre Live</strong><br />Adult: $25<br />Concession: $20</p>
+<p>&nbsp;</p>
+<p><em>PLEASE NOTE: NZ law prohibits the screening of restricted movies at sessions&nbsp;attended by any person under the age designated in the Censorship Classification.</em></p>
+<p class="h1"><code></code></p></div><footer><div id="text"><p>Web design and content &copy; 2019, Shoreline Cinema Waikanae, New Zealand. <a id="improve-visibility">Improve Visibility.</a></p></div></footer></div><script src="/tpl/inc/js/jquery-3.4.1.min.js"></script><script src="/tpl/inc/js/js.cookie-2.2.1.min.js"></script><script src="/tpl/inc/js/scripts.js"></script><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-137475424-2"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-137475424-2');</script></body></html>
+<?php }
+}
