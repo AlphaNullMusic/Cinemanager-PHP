04415a36b50633390414ea002837dbcca6f9bc2b
Roman Avery <roman@cinemanager.co.nz>
2022-08-12 12:11:40 +1200

    Update ignore file.

---
+++ web/tpl/cache/movie_332^3a7a1342f0e02bdf4cf5f1e327d152fd796808eb.movies.tpl.php
@@ -0,0 +1,119 @@
+<?php
+/* Smarty version 3.1.33, created on 2022-08-12 12:06:49
+  from '/var/www/Cinemanager/web/tpl/movies.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_62f59999d9c0b4_25115393',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '7e731f8444ca3c4d387cec6c94cedd5afa32db57' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/movies.tpl',
+      1 => 1621467589,
+      2 => 'file',
+    ),
+    '12f3ac630680bc6778753de8bb98e13015ec9d16' => 
+    array (
+      0 => '/var/www/Cinemanager/web/tpl/inc/tpl/footer.tpl',
+      1 => 1578300179,
+      2 => 'file',
+    ),
+  ),
+  'cache_lifetime' => 600,
+),true)) {
+function content_62f59999d9c0b4_25115393 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="en-nz">
+<head>
+<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>The Princess - Info</title>
+<meta name="description" content="Shoreline Cinema is Waikanae's premiere cinema complex. Find out who we are, what's on, what's coming up, how to contact us and much more.">
+<link href="/tpl/inc/css/styles.css" rel="stylesheet" type="text/css" />
+<link href="/tpl/inc/css/print.css" rel="stylesheet" type="text/css" media="print" />
+</head>
+<body>
+    <header>  
+      <div class="box-auto hide show-med"><img src="/tpl/inc/img/sl_logo.png" class="img-responsive mc-auto show"></div>
+      
+      <nav>
+        <a class="logo hide-med" href="#"><img src="/tpl/inc/img/sl_logo.png" height="25"></a>
+        <button class="hide-med" type="button">
+            <span class="icon-text">Menu&nbsp;</span>
+            <span class="icon"></span>
+        </button>
+        
+        
+        <div class="collapse jc-center" id="navbar">
+            <ul class="nav-links">
+                <li class="nav-item">
+                    <a class="nav-link " href="/home/">HOME</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/whats-on-today/">WHAT&apos;S ON TODAY</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/whats-on/">WHAT&apos;S ON</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/coming-soon/">COMING SOON</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/venue-hire/">VENUE HIRE</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/about-us/">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/contact-us/">CONTACT US</a>
+                </li>
+            </ul>
+        </div>
+      </nav>
+    </header>
+    
+    <div class="wrapper">
+			
+
+
+<div class="information">
+    <h2>The Princess</h2>
+    <div class="content">
+    	<div class="content-wrapper poster">
+    		<img src="https://posters.shoreline.nz/332-medium-custom.jpg" width="190" alt="The Princess Poster" />
+    	</div>
+    	<div class="content-wrapper text"> 
+    		<div class="content">
+    		    <div class="content-wrapper text">
+    		        <p>	The story of the marriage of Diana and Charles, the Prince and Princess of Wales, has been well documented. The fractured relationship that followed their seemingly fairy-tale public courtship and wedding in the early 1980s, the birth of two heirs to the throne, and Diana’s tragic and untimely death on August 31st, 1997, were tabloid fodder for nearly two decades and nearly made daily headlines in the 24-hour news cycle. THE PRINCESS aims to reframe this story through an entirely immersive approach by drawing solely from contemporaneous archival audio and video footage to take audiences back to these era-defining events as they happened, and in doing so, allows the narrative to unfold as if it were in the present. THE PRINCESS offers an account of Diana’s story – and the public response to it – that feels at once both unmediated and intensely intimate and emotional. The film stands as both a factual reckoning and a visceral submersion into Diana’s life in the constant, overpowering, and often intrusive glare of the media spotlight. Through archival material, including audio of the public response to these events, the film also provides a reflection of society at the time. Audiences see how the overwhelming adoration, but also intense scrutiny of Diana’s every move and the constant judgement of her character, reveal the public’s own preoccupations, fears, aspirations and desires.							</p>
+					    		                                        <span class="h3">Upcoming Screening Times</span>
+                            <ul class="sessions">
+                                                            	                              	
+                              	<li><strong>Monday 15 Aug</strong><a href="/bookings/10591/">4:00pm</a>                              	                              	
+                              	<li><strong>Wednesday 17 Aug</strong><a href="/bookings/10592/">3:15pm</a>                               </li>
+                            </ul>
+                          	<p><i>To place a booking, click on the session time you are interested in. Bookings must be made an hour before the film starts. Please wait for confirmation from us via phone or email.</i></p>
+                            		    </div>
+    		    <div class="content-wrapper details">
+    		                			<p>
+        				<strong>Rated:</strong> E<br />
+					        				 <em>Suitable for Everyone</em>        			</p>
+        		    			        		    <p>
+        			        			        			        				<a class="btn dark" href="https://www.youtube.com/watch?v=vxLzAWMHhdE" target="_blank">Official Trailer</a>
+        			        		    </p>
+            		  
+    		    </div>
+    		</div>
+    	</div>
+      </div> 
+    </div>
+
+<footer><div id="text"><p>Web design and content &copy; 2022, Shoreline Cinema Waikanae, New Zealand. <a id="improve-visibility">Improve Visibility.</a></p></div></footer></div><script src="/tpl/inc/js/jquery-3.4.1.min.js"></script><script src="/tpl/inc/js/js.cookie-2.2.1.min.js"></script><script src="/tpl/inc/js/scripts.js"></script><!-- Global site tag (gtag.js) - Google Analytics --><script async src="https://www.googletagmanager.com/gtag/js?id=UA-137475424-1"></script><script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-137475424-1');</script></body></html>
+
+
+<?php }
+}
